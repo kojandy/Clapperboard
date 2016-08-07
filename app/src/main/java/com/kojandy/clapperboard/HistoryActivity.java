00519afab057708256e8d6183ae43e409f5d6b83
kojandy <kojandy@gmail.com>
2016-08-08 03:46:13 +0900

    data edit function

--- app/src/main/java/com/kojandy/clapperboard/HistoryActivity.java
+++ app/src/main/java/com/kojandy/clapperboard/HistoryActivity.java
@@ -1,10 +1,13 @@
 package com.kojandy.clapperboard;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import io.realm.Realm;
@@ -18,19 +21,66 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
         ListView listView = (ListView) findViewById(R.id.history_list);
-        final RealmResults<Data> data = Realm.getDefaultInstance().where(Data.class).findAll();
+        final RealmResults<Data> data = Realm.getDefaultInstance().where(Data.class)
+                .findAllSorted(new String[]{"scene", "cut", "take"},
+                        new Sort[]{Sort.ASCENDING, Sort.ASCENDING, Sort.ASCENDING});
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, final int position, long l) {
                 Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
                     @Override
                     public void execute(Realm realm) {
-                        data.deleteFromRealm(position);
+                        AlertDialog.Builder alert = new AlertDialog.Builder(HistoryActivity.this);
+
+                        alert.setTitle("편집");
+                        alert.setMessage("메모할 내용을 아래에 입력해서 변경하거나 OK를 토글하거나 데이터를 지울 수 있어");
+
+                        final EditText input = new EditText(HistoryActivity.this);
+                        input.setText(data.get(position).note);
+                        alert.setView(input);
+
+                        alert.setPositiveButton("변경", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                final String value = input.getText().toString();
+                                Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+                                    @Override
+                                    public void execute(Realm realm) {
+                                        data.get(position).note = value;
+                                    }
+                                });
+                            }
+                        });
+
+
+                        alert.setNegativeButton("토글",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+                                            @Override
+                                            public void execute(Realm realm) {
+                                                data.get(position).isOk = !data.get(position).isOk;
+                                            }
+                                        });
+                                    }
+                                });
+
+                        alert.setNeutralButton("삭제", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+                                    @Override
+                                    public void execute(Realm realm) {
+                                        data.deleteFromRealm(position);
+                                    }
+                                });
+                            }
+                        });
+
+                        alert.show();
                     }
                 });
             }
         });
-        listView.setAdapter(new Adapter(this, data.sort(new String[]{"scene", "cut", "take"},
-                new Sort[]{Sort.ASCENDING, Sort.ASCENDING, Sort.ASCENDING})));
+        listView.setAdapter(new Adapter(this, data));
     }
 }
